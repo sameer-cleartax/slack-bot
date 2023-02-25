82cec430ae359e8bb541dfdb132e4e4ce9fb5b6c
Sameer Choudhary <108799759+sameer-cleartax@users.noreply.github.com>
2023-02-25 21:35:16 +0530

    final version v1

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,14 @@
-import * as core from '@actions/core';
-import { prService } from './service/pr.service';
+import * as core from "@actions/core";
+import { prService } from "./service/pr.service";
 
 async function run() {
-    try {
-        const webhook:string = core.getInput("webhook");
-        const tag:string[] = core.getInput("tag").split(',');
-        prService(webhook, tag);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const webhook: string = core.getInput("webhook");
+    const tag: string[] = core.getInput("tag").split(",");
+    prService(webhook, tag);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 run();
-
